36a3a561fa549f698177c5095b52a26c68b0fe25
Mikhail Krasilnik <brazik.omsk@mail.ru>
2021-02-06 10:25:41 +0600

    Initial Commit

---
+++ ToDoWithCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoWithCoreData
+//
+//  Created by Михаил Красильник on 06.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
